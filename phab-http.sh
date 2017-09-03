8a1bdb223c97ae4d976a216659d86379e9a6a638
Sean Enck <enckse@gmail.com>
2017-09-03 16:17:21 -0400

    no longer going to use temp file placement

--- phab-http.sh
+++ phab-http.sh
@@ -4,11 +4,4 @@ source $VARS
 for v in $(cat $VARS | grep -v "^#" | cut -d "=" -f 1); do
     export $v
 done
-PATH_TO="/opt/epiphyte/phab-http"
-HOOK=${PATH_TO}/phab-http
-TMP_HOOK=${PATH_TO}/.phab-http
-if [ -e $TMP_HOOK ]; then
-    mv $TMP_HOOK $HOOK
-fi
-
-$HOOK
+phab-http
